cad13765113cc8ba5ae7f784f37e33094e7d5eaa
plkristina <145187075+plkristina@users.noreply.github.com>
2023-11-15 14:16:11 +0300

    Create task11.cpp

---
+++ 1/task11.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (abs(a - b) > abs(a - c)) {
+        cout << "C " << abs(a - c);
+    }
+    else {
+        cout << "B " << abs(a - b);
+    }
+    return 0;
+}
