36f35687b6655fa93753277e4c1fd470937a3330
plkristina <145187075+plkristina@users.noreply.github.com>
2023-11-15 13:33:17 +0300

    Create main.cpp

---
+++ 1/1/main.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+#include <cmath>
+
+int main(){
+    float q = (1 - (1.0 / 9) + (1.0 / 45) - (1.0 / (7 * 27)) + (1.0 / (9 * 81)) - (1.0 / (11 * 243)));
+    std::cout << q * sqrt(12);
+    return 0;
+}
